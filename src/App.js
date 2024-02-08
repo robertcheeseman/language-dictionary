a88c955a936ff31bb5f17dfeb200ccf3bcb22039
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-08 10:01:34 -0700

    added api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane");
+      setMeanings(data.data);
+
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
     <div className="App">
-    Hello World
+    Dictionary
     </div>
   );
 }
