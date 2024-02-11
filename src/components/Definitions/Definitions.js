bb5e4eec8b908f385f507b421bb7e8b4024ba16f
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-10 23:18:13 -0700

    bug fix

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -4,11 +4,8 @@ import './Definitions.css';
 const Definitions = (word, category, meanings) => {
   return (
     <div className='meanings'>
-        {word === "" ? (
-            <span className='subTitle'>Start by typing a word in the search box</span>
-        ) : ( 
-            meanings.map((mean) => mean.meanings.map((item) => console.log(item)))
-         )} 
+
+        
     </div>
   );
 };
