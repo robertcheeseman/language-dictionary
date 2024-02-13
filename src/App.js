1e522f4d0a8c790ee1e21a1e8e282592a835f645
Robert Cheeseman <robert.s.cheeseman@gmail.com>
2024-02-13 10:55:58 -0700

    debugging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import { Container } from '@mui/material';
 import axios from "axios";
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import "./App.css";
 import Definitions from "./components/Definitions/Definitions";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-
 
 function App() {
   const [word, setWord] = useState("");
   const [meanings, setMeanings] = useState([]);
-  const [category, setCategory] = useState("en")
+  const [category, setCategory] = useState("en");
 
-  const dictionaryApi = async() => {
+  const dictionaryApi = async () => {
     try {
-      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
       setMeanings(data.data);
     } catch (error) {
-      console.log(error)     
+      console.log(error);
     }
-  }
-  console.log(word)
+  };
+
   console.log(meanings);
 
   useEffect(() => {
@@ -27,20 +29,46 @@ function App() {
     // eslint-disable-next-line
   }, [word, category]);
 
-  return (
-    <div className="App" style={{height:'100vh', backgroundColor:"grey", color: 'white'}}>
-    <Container maxWidth="md" style={{display:"flex", flexDirection: "column", height:"100vh"}}>
-    
-      <Header setWord={setWord} category={category} setCategory={setCategory} word={word} />
-     {meanings && (
-     <Definitions word={word} meanings={meanings} category={category}/>
-     )}
-     <p>This is the word: {word}</p>
-     <p>This is the category: {category}</p>
 
-    </Container>
-    
-    </div> 
+  return (
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+
+        </div>
+        <Header
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+        />
+        {meanings && (
+          <Definitions
+            meanings={meanings}
+            word={word}
+            category={category}
+          />
+        )}
+      </Container>
+      <Footer />
+    </div>
   );
 }
 
